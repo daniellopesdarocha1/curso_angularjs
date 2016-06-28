c62f8370423e71b1043dd22775b518d28d3a0bf9
Daniel Lopes da Rocha <daniel.rocha@yros.com.br>
2016-06-28 16:29:29 -0300

    Utilizando controllers

---
+++ js/aula06Controller.js
@@ -0,0 +1,24 @@
+
+app.controller('aula06Controller', function($scope){
+
+	$scope.nome = "Curso AngularJS Devmedia";
+
+	$scope.olaMundo = function(nome){
+
+		alert("Olá " + nome + "!");
+
+	}
+
+	console.log("Executou o controller aula06Controller ")
+});
+
+/*
+o scope é uma variavel onde vão ficar armazenadas 
+todas as variaveis e funções
+*/
+
+/*
+Controllers - controller é um trecho de código que controla a app. 
+Os controllers são separados em arquivos diferentes,
+e eles podem conter funcionalidades como funções e regras de negócios.
+*/
